1f5d529152bbe4165217b902f30e2231fc13c847
mconr <moniruar05@gmail.com>
2023-03-05 23:28:17 +0100

    Ex1 from hackerrank tests

---
+++ Ex 01 .py
@@ -0,0 +1,18 @@
+# First test from hackerRank 
+import math
+import os
+import random
+import re
+import sys
+
+def reversewordSentence( phrase ):
+    word = phrase.split()
+    revlist = word[::-1]
+    revphrase = " ".join(revlist)
+    return revphrase.swapcase()
+
+
+print('write it amigoo')
+phrase = input()
+print(phrase)
+print(reversewordSentence(phrase))
